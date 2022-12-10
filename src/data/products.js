d47c0604c121b8c66b5a55c03f7e05a238c0d000
TsukiHyaku <alexander.lamas.2003@gmail.com>
2022-12-10 16:34:59 -0300

    añadido de productos

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,12 @@
 const products = [
   {
     id: 1, 
-    title: "Funko Pop Gojo Jujutsu Kaisen",
+    title: "Evangelion: 3.0+1.0 – Rei Ayanami",
     price: 2500,
     stock: 7,
     category: "figura",
-    img: "https://criticalhit.com.au/wp-content/uploads/2022/05/FUN62337-_JJK_SatoruGojoRed_POP_GLAM-WEB-200x298.jpeg",
-    description: "esto es un funko",
+    img: "https://criticalhit.com.au/wp-content/uploads/2022/07/rei_3-200x224.jpg",
+    description: "esto es un Figura",
   },
 
   {
@@ -27,6 +27,96 @@ const products = [
     category: "manga",
     img: "https://http2.mlstatic.com/D_NQ_NP_733376-MLA48233994693_112021-V.webp",
     description: "esto es un manga",
+  },
+
+  {
+    id: 4, 
+    title: "Kimetsu no Yaiba Vol.10",
+    price: 1200,
+    stock: 8,
+    category: "manga",
+    img: "https://http2.mlstatic.com/D_NQ_NP_731778-MLA47384343008_092021-V.webp",
+    description: "esto es un manga",
+  },
+
+  {
+    id: 5, 
+    title: "Dragon Head Vol.04",
+    price: 2799,
+    stock: 12,
+    category: "manga",
+    img: "https://http2.mlstatic.com/D_NQ_NP_794333-MLA52481874055_112022-V.webp",
+    description: "esto es un manga",
+  },
+
+  {
+    id: 6, 
+    title: "Nagatoro Vol.06",
+    price: 1200,
+    stock: 3,
+    category: "manga",
+    img: "https://http2.mlstatic.com/D_NQ_NP_667329-MLA52650429599_112022-V.webp",
+    description: "esto es un manga",
+  },
+
+  {
+    id: 7, 
+    title: "Monkey D Luffy 1",
+    price: 5800,
+    stock: 20,
+    category: "buzo",
+    img: "https://http2.mlstatic.com/D_NQ_NP_960184-MLA50941468734_072022-W.webp",
+    description: "esto es un manga",
+  },
+
+  {
+    id: 8, 
+    title: "Fase Lunar Aesthetic",
+    price: 5399,
+    stock: 32,
+    category: "buzo",
+    img: "https://http2.mlstatic.com/D_NQ_NP_614425-MLA49856617783_052022-W.webp",
+    description: "esto es un manga",
+  },
+
+  {
+    id: 9, 
+    title: "Darling - Buzo Canguro Unisex",
+    price: 5800,
+    stock: 7,
+    category: "buzo",
+    img: "https://http2.mlstatic.com/D_NQ_NP_604160-MLA49745966759_042022-W.webp",
+    description: "esto es un manga",
+  },
+
+  {
+    id: 10, 
+    title: "Haikyu!! To The Top – Tobio Kageyama POP UP PARADE",
+    price: 6298,
+    stock: 5,
+    category: "figura",
+    img: "https://criticalhit.com.au/wp-content/uploads/2021/05/000942706_06-200x281.jpg",
+    description: "esto es un Figura",
+  },
+
+  {
+    id: 11, 
+    title: "Jujutsu Kaisen 0 – Hikkake Figure",
+    price: 2100,
+    stock: 2,
+    category: "figura",
+    img: "https://criticalhit.com.au/wp-content/uploads/2022/06/4580736401587-200x200.jpg",
+    description: "esto es un Figura",
+  },
+
+  {
+    id: 12, 
+    title: "Demon Slayer: Kimetsu no Yaiba – Mitsuri Kanroji",
+    price: 3800,
+    stock: 1,
+    category: "figura",
+    img: "https://criticalhit.com.au/wp-content/uploads/2022/07/DemonSlayerKimetsunoYaibaPMPerchingPVCStatueMitsuriKanroji_1400x-200x200.png",
+    description: "esto es un Figura",
   },
 
 ];
